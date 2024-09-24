cf4f20f3137a3b61a3bfeb527806ee8d50d5a2e5
LIS <islagunov@edu.hse.ru>
2024-09-24 21:10:49 +0300

    + long везде + скобки

--- Lab2 C++.cpp
+++ Lab2 C++.cpp
@@ -1,32 +1,32 @@
-﻿#include <iostream>
+#include <iostream>
 #include<cmath>
 using namespace std;
 
-float t(float x) {
-    float v=0;
-    float n=0;
+long double t(long double x) {
+    long double v=0;
+    long double n=0;
     for (int i = 0; i <= 10; i++) {
-        int ans = 1;
+        long long ans = 1;
         for (int ii = 2 * i + 1; ii >= 1; ii = ii - 2) {
             ans = ans * ii;
         }
-        v = v + (pow(x,(2 * i + 1))) / ans;
+        v = v + ((pow(x,(2 * i + 1))) / ans);
     }
     for (int i = 0; i <= 10; i++) {
-        int ans = 1;
+        long long ans = 1;
             for (int ii = 2 * i; ii >= 1; ii = ii - 2) {
                 ans = ans * ii;
             }
-        n = n + (pow(x,(2 * i))) / ans;
+        n = n + ((pow(x,(2 * i))) / ans);
     }
     return v/n;
 }
 
 int main()
 {
-    float y;
+    long double y;
     cin >> y;
-    float ans;
+    long double ans;
     //cout << "t(x)" << t(0.25)<<endl;
     ans = (7 * t(0.25) + 2 * t(1 + y)) / (6 - t(y * y - 1));
     cout << ans;
