ad9ae97641ced00c7b6201c0c469e50182d9957f
LIS <islagunov@edu.hse.ru>
2024-09-24 20:25:43 +0300

    + file

---
+++ Lab2 C++.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+
+float t(float x) {
+    float v=0;
+    float n=0;
+    for (int i = 0; i <= 10; i++) {
+        int ans = 1;
+        for (int ii = 2 * i + 1; ii >= 1; ii = ii - 2) {
+            ans = ans * ii;
+        }
+        v = v + (pow(x,(2 * i + 1))) / ans;
+    }
+    for (int i = 0; i <= 10; i++) {
+        int ans = 1;
+            for (int ii = 2 * i; ii >= 1; ii = ii - 2) {
+                ans = ans * ii;
+            }
+        n = n + (pow(x,(2 * i))) / ans;
+    }
+    return v/n;
+}
+
+int main()
+{
+    float y;
+    cin >> y;
+    float ans;
+    //cout << "t(x)" << t(0.25)<<endl;
+    ans = (7 * t(0.25) + 2 * t(1 + y)) / (6 - t(y * y - 1));
+    cout << ans;
+    return 0;
+}
